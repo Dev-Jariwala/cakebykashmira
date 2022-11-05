2a4306d7219c8341d277a4333bf947750927878f
Dev-Jariwala <devjariwala.j@gmail.com>
2022-11-05 23:05:09 +0530

    like

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 
 // Scroll the flovours tab.
 
-let currentScrollPosition = 0;
-let scrollAmount = 320;
-const sCont = document.querySelector(".flav-items");
-const hScroll = document.querySelector(".horizontal-scroll");
-const btnScrollLeft = document.querySelector("#btn-scroll-left");
-const btnScrollRight = document.querySelector("#btn-scroll-right");
-
-btnScrollLeft.style.opacity = "0";
-
-let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
-
-function scrollHorizontally(val){
-  currentScrollPosition+=(val * scrollAmount);
-
-  if (currentScrollPosition >= 0) {
-    currentScrollPosition = 0;
-    btnScrollLeft.style.opacity = "0";
-  }else{
-    btnScrollLeft.style.opacity = "1";
-  }
-  if (currentScrollPosition <= maxScroll) {
-    currentScrollPosition = maxScroll;
-    btnScrollRight.style.opacity = "0";
-  }else {
-    btnScrollRight.style.opacity = "1";
-  }
-
-
-  sCont.style.left = currentScrollPosition + "px";
-
-}
+// let currentScrollPosition = 0;
+// let scrollAmount = 320;
+// const sCont = document.querySelector(".flav-items");
+// const hScroll = document.querySelector(".horizontal-scroll");
+// const btnScrollLeft = document.querySelector("#btn-scroll-left");
+// const btnScrollRight = document.querySelector("#btn-scroll-right");
+//
+// btnScrollLeft.style.opacity = "0";
+//
+// let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+//
+// function scrollHorizontally(val){
+//   currentScrollPosition+=(val * scrollAmount);
+//
+//   if (currentScrollPosition >= 0) {
+//     currentScrollPosition = 0;
+//     btnScrollLeft.style.opacity = "0";
+//   }else{
+//     btnScrollLeft.style.opacity = "1";
+//   }
+//   if (currentScrollPosition <= maxScroll) {
+//     currentScrollPosition = maxScroll;
+//     btnScrollRight.style.opacity = "0";
+//   }else {
+//     btnScrollRight.style.opacity = "1";
+//   }
+//
+//
+//   sCont.style.left = currentScrollPosition + "px";
+//
+// }
 
 // Like count.
 
